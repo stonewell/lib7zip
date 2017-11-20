3f7242a69338a242ed26b04287ee089e192b3994
Amos Wenger <amos@itch.io>
2017-11-20 16:54:20 +0100

    Fix calls to MultiByteToWideChar

--- Test7Zip/Test7Zip2.cpp
+++ Test7Zip/Test7Zip2.cpp
@@ -127,7 +127,7 @@ public:
 		{
 #ifdef _WIN32
 			std::string tmp = m_strFileName.substr(pos + 1);
-			int nLen = MultiByteToWideChar(CP_ACP, 0, tmp.c_str(), -1, NULL, NULL);
+			int nLen = MultiByteToWideChar(CP_ACP, 0, tmp.c_str(), -1, NULL, 0);
 			LPWSTR lpszW = new WCHAR[nLen];
 			MultiByteToWideChar(CP_ACP, 0, 
 			   tmp.c_str(), -1, lpszW, nLen);
