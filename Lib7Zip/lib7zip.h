869f96c15f0a644461900cec3b23c035a08a8303
Amos Wenger <amos@itch.io>
2017-11-22 12:44:41 +0100

    Remove version defines, don't use '-l' when linking against libs, so it compiles with msvc

--- Lib7Zip/lib7zip.h
+++ Lib7Zip/lib7zip.h
@@ -1,15 +1,6 @@
 #ifndef __LIB_7ZIP_H__
 #define __LIB_7ZIP_H__
 
-#define LIB_7ZIP_VER_MAJOR 2
-#define LIB_7ZIP_VER_MINOR 0
-#define LIB_7ZIP_VER_BUILD 0
-#define LIB_7ZIP_VERSION "2.0"
-#define LIB_7ZIP_7ZIP_VERSION "lib7Zip 2.0"
-#define LIB_7ZIP_DATE "2015-11"
-#define LIB_7ZIP_COPYRIGHT "Copyright (c) 2009-2015"
-#define LIB_7ZIP_VERSION_COPYRIGHT_DATE MY_VERSION "  " MY_COPYRIGHT "  " MY_DATE
-
 #include <string>
 #include <vector>
 
